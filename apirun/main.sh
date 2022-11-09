f7b99a3ce8ccc91063b1e99319a0b7fd90a505aa
StefanGary <sfgary@parallelworks.com>
2022-11-09 15:41:09 +0000

    Small tweaks to workflow params in API example

--- apirun/main.sh
+++ apirun/main.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+#===========================
+# It is essential that the
+# workflow parameters (all
+# denoted with the "commands|"
+# prefix in the JSON string)
+# are exactly correct.  Otherwise,
+# the workflow will fail without
+# any direct error messages to
+# the user.
+#==========================
 ${CONDA_PYTHON_EXE} run_workflow.py \
     ${PARSL_CLIENT_HOST} \
     ${PW_API_KEY} \
     ${PW_USER} \
     gcev2 \
     ssh_bash_demo \
-    '{"commands|whost": "gcev2.clusters.pw", "commands|rundir": "/var", "commands|runcmd": "ls", "commands|sleep_time": "1", "commands|head_or_worker": "false"}'
+    '{"commands|whost": "gcev2.clusters.pw", "commands|rundir": "/var", "commands|runcmd": "ls", "commands|spaces_in_runcmd": "False", "commands|sleep_time": "3", "commands|head_or_worker": "False"}'
