bdeac10297774c07fd0392b76d0816de140fbd0b
StefanGary <sfgary@parallelworks.com>
2022-11-16 16:14:11 +0000

    Changed default resource to cloud
    and added newlines in workflow.xml for readability.

--- apirun/main.sh
+++ apirun/main.sh
@@ -13,6 +13,6 @@ ${CONDA_PYTHON_EXE} run_workflow.py \
     ${PARSL_CLIENT_HOST} \
     ${PW_API_KEY} \
     ${PW_USER} \
-    gcev2 \
+    cloud \
     ssh_bash_demo \
-    '{"commands|whost": "gcev2.clusters.pw", "commands|rundir": "/var", "commands|runcmd": "ls -a -l -h", "commands|spaces_in_runcmd": "True", "commands|sleep_time": "10", "commands|head_or_worker": "False"}'
+    '{"commands|whost": "cloud.clusters.pw", "commands|rundir": "/var", "commands|runcmd": "ls -a -l -h", "commands|spaces_in_runcmd": "True", "commands|sleep_time": "10", "commands|head_or_worker": "True"}'
