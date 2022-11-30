7c4a4d6d4ff6dbab40b44b19bdb5d665c035c490
stefangary <40672572+stefangary@users.noreply.github.com>
2022-11-30 11:41:47 -0500

    Add API run documentation
    Clarified the requirements to API launch a job on PW.

--- apirun/main.sh
+++ apirun/main.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 #===========================
+# This script is a wrapper 
+# to document how inputs are 
+# sent to run_workflow.py.
+# 
+# If you do not have a Conda
+# environment installed, then
+# you can replace "${CONDA_PYTHON_EXE}"
+# with "python".
+#
+# The key parameters are:
+# 1) Where to run the workflow, e.g.
+#    on cloud.parallel.works
+# 2) Your API key (a sort of password)
+# 3) Your PW account user name
+# 4) Name of the PW resource to run
+#    this workflow on (set up in 
+#    the Resources tab).
+# 5) Name of the workflow to run
+#    (must already be setup on PW)
+# 6) Workflow parameters that correspond
+#    to the workflow inputs in workflow.xml
+#    for the workflow specified in #5.
+#
 # It is essential that the
 # workflow parameters (all
 # denoted with the "commands|"
